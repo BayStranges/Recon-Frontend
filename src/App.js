d301ef99d7ad038bf129e4d5b287e7c0e0130eb4
BayStranges <emircantari01@gmail.com>
2024-12-26 00:51:47 +0300

    Düzenlemeler Yapıldı 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
 import Profile from './components/Profile'; // Profile bileşenini import ettik
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home'; // Ana sayfa
 
@@ -12,17 +11,19 @@ function App() {
 
   return (
     <Router>
-    {/* Routes bileşeni ile yolları tanımlıyoruz */}
-    <Routes>
-      {/* Login sayfası */}
-      <Route path="/login" element={<Login />} />
-      {/* Profil sayfası */}
-      <Route path="/profile" element={<Profile />} />
-      {/* Ana sayfa */}
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </Router>
-);
+      {/* Routes bileşeni ile yolları tanımlıyoruz */}
+      <Routes>
+        {/* Login sayfası */}
+        <Route path="/login" element={<Login />} />
+        {/* Profil sayfası */}
+        <Route path="/profile" element={<Profile />} />
+        {/* Login sayfası */}
+        <Route path="/register" element={<Register />} />
+        {/* Ana sayfa */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
